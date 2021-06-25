977f36596b9535840eef5226f8aa6219eb1f6a1a
rkendre <rameshwar.kendre@synerzip.com>
2021-06-25 18:45:33 +0530

    applied fix for sqlcm-6393

--- development/Idera/SQLcompliance/Application/GUI/Controls/DefaultSettings.Designer.cs
+++ development/Idera/SQLcompliance/Application/GUI/Controls/DefaultSettings.Designer.cs
@@ -46,6 +46,7 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.AutoScroll = true;
             this.tabPage1.Location = new System.Drawing.Point(4, 24);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -57,10 +58,11 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.AutoScroll = true;
             this.tabPage2.Location = new System.Drawing.Point(4, 24);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(749, 477);
+            this.tabPage2.Size = new System.Drawing.Size(769, 477);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Databases";
             this.tabPage2.UseVisualStyleBackColor = true;
